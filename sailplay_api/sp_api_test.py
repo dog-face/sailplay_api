ab121ffef9b6f07d7179382b1ca3dd649072c25b
Noah Vito <noah.a.vito@gmail.com>
2017-03-03 11:56:49 -0500

    first commit

---
+++ sailplay_api/sp_api_test.py
@@ -0,0 +1,18 @@
+import sp_api_connector
+
+api_connector = sp_api_connector.api_connector(XXXX, XXXXXXXX, XXXX)
+
+print("Logging in")
+token = api_connector.login()
+if token is not False:
+	print("\tToken: %s " % token)
+else:
+	print("\tError")
+	exit(1)
+
+print("Getting info")
+user_info = api_connector.users_info(phone="XXXXXXXXXXX")
+if user_info is not False:
+	print("\t%s" % user_info)
+else:
+	print("\tError")
