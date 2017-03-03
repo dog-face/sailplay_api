08ea234a17bf0c3333823ff3afb5de2916d38208
Noah Vito <noah.a.vito@gmail.com>
2017-03-03 12:21:26 -0500

    new gitignore rules

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
 setup(name='sailplay_api',
-      version='0.2',
+      version='0.3',
       description='A Python wrapper for the SailPlay API',
       url='https://github.com/mznco/sailplay_api',
       author='mznco',
       author_email='admin@mznco.net',
       license='GPL-3.0',
       packages=['sailplay_api'],
+	  install_requires=[
+		'urllib',
+		'urllib2',
+		'logging',
+		'json',
+	  ],
       zip_safe=False)
